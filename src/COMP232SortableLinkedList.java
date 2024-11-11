d856a9a393c595d83f0f53dda945d78e9fe2aa70
Anastasiia <halchena@dickinson.edu>
2024-11-11 09:05:00 -0500

    insertionSort

--- src/COMP232SortableLinkedList.java
+++ src/COMP232SortableLinkedList.java
@@ -1,3 +1,5 @@
+
+
 /**
  * A sortable DoublyLinkedList.  The fields and the DLLNode class in the
  * COMP232DoublyLinkedList class are protected fields and can be accessed
@@ -16,6 +18,35 @@ public class COMP232SortableLinkedList<E extends Comparable<E>> extends COMP232D
 	 */
 	public void insertionSortList() {
 		// Intentionally not implemented - see homework assignment.
-		throw new UnsupportedOperationException("Not yet implemented");
+		//throw new UnsupportedOperationException("Not yet implemented");
+		
+		for (int i = 1; i < size; i++) {
+	        int j = i;
+	        while (j > 0 && get(j - 1).compareTo(get(j)) > 0) {
+	     
+	            E node1 = get(j - 1);
+	            E node2 = get(j);
+	            set(j - 1, node2);
+	            set(j, node1);
+	            j--;
+	        }
+		}
+	
+	}
+	
+	public static void main(String[] args) {
+		COMP232SortableLinkedList<Integer> list = new COMP232SortableLinkedList<Integer>();
+		list.add(2);
+		list.add(3);
+		list.add(1);
+		list.add(2);
+		list.add(2);
+		list.add(3);
+		list.add(1);
+		
+		list.insertionSortList();
+		System.out.println(list.toString());
+		
+		
 	}
 }
